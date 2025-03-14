e93dbbc12ab236c1c5d14f951a4e2abde18a979e
CADIC Jean Maximilien <j.cadic@protonmail.ch>
2025-03-14 15:38:50 +0900

    fix mac sed issue

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import re
 
 from setuptools import find_packages, setup
 
-with open("pyproject.toml", encoding='utf-8')  as file:
+with open("pyproject.toml")  as file:
     setup(
         version=re.search(
-            r'^version\s*=\s*"(.*?)"', file.read(), re.M
+            r'^version\s*=\s*"(.*?)"',  file.read(), re.M
         ).group(1),
         packages=find_packages(),
     )
